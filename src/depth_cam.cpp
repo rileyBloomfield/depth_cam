01c90affa072455de2851fd64da0e5fd6c26eb65
ROSbox <rosbox@rosbox-desktop.(none)>
2015-06-10 15:45:40 -0400

    Added some data retrieval functions

--- src/depth_cam.cpp
+++ src/depth_cam.cpp
@@ -5,6 +5,9 @@
 #include "stdlib.h"
 #include <ctime>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 // on error, prepend absolute path to files before plugin names
 #define SOURCE_PLUGIN "camboardnano"
@@ -14,20 +17,87 @@
 
 char err[128];
 
-int main(int argc, char** argv) {
-
-PMDHandle hnd;
+class Camera {
+  private:
+    PMDHandle camHandle;
     int res;
-
-    res = pmdOpen (&hnd, SOURCE_PLUGIN, SOURCE_PARAM, PROC_PLUGIN, PROC_PARAM);
-    if (res != PMD_OK)
-    {
+  public:
+    Camera() {
+	//Open Camera
+    res = pmdOpen (&camHandle, SOURCE_PLUGIN, SOURCE_PARAM, PROC_PLUGIN, PROC_PARAM);
+    if (res != PMD_OK) {
         pmdGetLastError (0, err, 128);
         fprintf (stderr, "Could not connect: %s\n", err);
-        return 1;
     }
-ros::Rate rate(5); //5Hz
-    printf ("opened sensor\n");
-    std::cout<<"Test"<<std::endl;
-    return 0;
+    std::cout<<"Camera was opened"<<std::endl;
+    };
+    ~Camera() {
+	//Close Camera
+    res = pmdClose(camHandle);
+    if (res != PMD_OK) {
+	pmdGetLastError (0, err, 128);
+        fprintf (stderr, "Could not close: %s\n", err);
+    }
+    std::cout<<"Camera was closed"<<std::endl;
+    };
+
+void update() {
+    //Update Camera
+    res = pmdUpdate(camHandle);
+    if(res != PMD_OK) {
+	printf ("Could not retrieve data\n");
+    }
+};
+
+void getDescription() {
+	//Get Camera Description
+    PMDDataDescription dataDesc;
+    res = pmdGetSourceDataDescription(camHandle, &dataDesc);
+    if (res != PMD_OK) {
+	printf("Could not get camera description\n");
+    }
+    std::cout<<"Sensor resolution: "<<dataDesc.img.numColumns<<"x"<<dataDesc.img.numRows<<std::endl;
+}
+
+void getZCoords() {
+	//Get Distances
+    float data[300000] = {};
+    size_t maxLen = 300000;
+    res = pmdGet3DCoordinates(camHandle, data, maxLen);
+    if (res != PMD_OK) {
+	printf("Error retrieving coords\n");
+    }
+    ofstream file;
+    file.open("z-coords.csv"); //Name output file
+    int index = 0;
+    for(int i=0; i<120; i++) {
+	for(int j=0; j<165; j++) {
+	    index +=2;
+	    if(data[index] > 0.3)
+		data[index] = 1.0;
+	    if (j == 164) {
+		file<<data[index++];
+	    }
+	    else {
+	    file<<data[index++]<<",";
+	    }
+	}
+	file<<"\n";
+    }
+}
+void getDistances() {
+}
+void getAmplitudes() {
+}
+void printFiles() {
+	getZCoords();
+	getDistances();
+	getAmplitudes();
+}
+};
+
+int main(int argc, char** argv) {
+	Camera cam = Camera();
+	cam.update();
+
 }
